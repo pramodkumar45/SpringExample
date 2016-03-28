d35e04f8153f3e78ae5417ba508ecbeda02edbf3
pramodkumar <pramodkumar_gadde@infosys.com>
2016-03-28 12:44:42 +0530

    hi

--- src/main/java/com/javahash/spring/config/Config.java
+++ src/main/java/com/javahash/spring/config/Config.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
 @Configuration //Marks this class as configuration
 //Specifies which package to scan
 @ComponentScan("com.javahash.spring")
-//Enables Spring's annotations 
+//ffEnables Spring's annotations 
 @EnableWebMvc   
 public class Config {  
       
